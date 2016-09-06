1189743e3278ae59fd67db0a4a0992f3618b857a
Arvind <arvind687@gmail.com>
2016-09-06 20:47:54 +0530

    Fixed issue where number of ways exceeded int limit, changed to long long int

--- coinchange.cpp
+++ coinchange.cpp
@@ -1,30 +1,34 @@
 #include "iostream"
 
 #define REP(i, n) for(int i = 0; i < n; i++)
-#define N 20
-#define M 20
+#define N 251
+#define M 51
 
 using namespace std;
 
-int dp[N][M];
-int numWaysChange(int n, int coins[], int m) {
+// for m, pass index of last element in coins array
+long long int dp[N][M];
+long long int numWays(int n, int coins[], int m) {
 	if (n < 0 || m < 0) return 0;
 	if (n == 0) return 1;
+
 	if (dp[n][m] != -1) return dp[n][m];
-	dp[n][m] = numWaysChange(n, coins, m - 1) + numWaysChange(n - coins[m], coins, m);
+
+	// number of ways by using mth coin + number of ways by not using mth coin 
+	dp[n][m] = numWays(n - coins[m], coins, m) + numWays(n, coins, m - 1);
 	return dp[n][m];
 }
 
 int main()
 {
-	int n = 4;
-	int coins[] = {1, 2, 3, 4};
-	int m = sizeof(coins)/sizeof(coins[0]);
+	int n, m;
+	cin >> n >> m;
+	int coins[m];
+	for(int i = 0; i < m; i++) cin >> coins[i];
 
 	for(int i = 0; i <= n; i++)
-		for(int j = 0; j <= m; j++) 
-			dp[i][j] = -1;
+		for(int j = 0; j <= m; j++) dp[i][j] = -1;
 
-	cout << numWaysChange(n, coins, m - 1) << '\n';
+	cout << numWays(n, coins, m - 1) << '\n';	
 	return 0;
 }
